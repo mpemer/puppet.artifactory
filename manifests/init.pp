900129f58d724b747986d0686139adfbd3f30ec8
Marcus Pemer <marcus@iteego.com>
2012-11-02 18:53:47 +0100

    ongoing work

--- manifests/init.pp
+++ manifests/init.pp
@@ -65,7 +65,7 @@ class artifactory {
     file { 'artifactory.war':
       path => '/var/lib/tomcat7/webapps/artifactory.war',
       source => "puppet:///artifactory-2.6.7.war",
-      require => File['artifactory_home_var'],
+      require => Line['artifactory_home_var'],
     }
 
   }
